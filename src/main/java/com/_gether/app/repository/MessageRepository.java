996ff7b11b445f6719dbd7bbe8da6cbad3b5be6e
Jose Guzman <josel.guzmand2013@gmail.com>
2025-10-20 17:01:33 -0600

    Update: Messages

--- src/main/java/com/_gether/app/repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com._gether.app.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com._gether.app.model.Message;
-
-public interface MessageRepository extends CrudRepository<Message, Long>{
-
-}
